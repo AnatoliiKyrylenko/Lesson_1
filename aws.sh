c9a299041d7dcc3bfd6c604128c85a245d12d606
Anatolii Kyrylenko <kyrylenkoanatolii@gmail.com>
2023-05-14 15:24:18 +0300

    aws script finished

--- aws.sh
+++ aws.sh
@@ -14,3 +14,10 @@ aws ec2 deregister-image --image-id $AMIOld
 # Set Instance region
 aws configure set default.region eu-central-1
 # Get Image Id from Name
+AMINew=$(aws ec2 describe-images --filters "Name=name,Values=image_name" --query 'Images[*].[ImageId]' --output text)
+# Copy Image to another region
+aws ec2 copy-image --source-image-id $AMINew --source-region eu-central-1 --region eu-west-3 --name "image_name"
+# Wait 5 minutes
+sleep 5m
+# Deregister image in instance region
+aws ec2 deregister-image --image-id $AMINew
